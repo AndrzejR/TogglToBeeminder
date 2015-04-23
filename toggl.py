b708c2e04d508a80d615c5244e96c5b721f1fb28
AndrzejR <andrzej.ruszczewski@gmail.com>
2015-04-23 20:23:51 +0200

    get rid of token from source

--- toggl.py
+++ toggl.py
@@ -1,8 +1,11 @@
 import requests
 import json
 
+api_token = None
+with open('token.txt', 'r') as f:
+	api_token = f.readline()
 
-r = requests.get('https://www.toggl.com/api/v8/me', auth=('59516aa742fa8eb8d29dd596d489448b', 'api_token'))
+r = requests.get('https://www.toggl.com/api/v8/me', auth=(api_token, 'api_token'))
 
 print(json.dumps(r.json(), sort_keys=True,
 				indent=4, separators=(',',':')))
