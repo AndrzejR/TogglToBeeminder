4f4adfdd4ff95b42c75f4b5948acb8301f2cddb1
AndrzejR <andrzej.ruszczewski@gmail.com>
2015-04-25 09:07:39 +0200

    Dynamically yesterday's date, just the hours

--- toggl.py
+++ toggl.py
@@ -1,23 +1,27 @@
 import requests
 import json
+from datetime import date, timedelta
 
 api_token = None
 with open('token.txt', 'r') as f:
 	api_token = f.readline()
 
+yesterday = date.today() - timedelta(days=1)
+
+yesterday = yesterday.isoformat()
+
+print(yesterday)
+
 payload = {'user_agent':'andrzej.ruszczewski@gmail.com', 'workspace_id':644218,
-			 'since':'2015-04-24', 'until':'2015-04-24', 'grouping':'clients',
+			 'since':yesterday, 'until':yesterday, 'grouping':'clients',
 			 'subgrouping':'projects'}
 
 r = requests.get('https://www.toggl.com/reports/api/v2/summary', auth=(api_token, 'api_token'), params=payload)
 
-print(json.dumps(r.json(), sort_keys=True,
-				indent=4, separators=(',',':')))
+# print(json.dumps(r.json(), sort_keys=True, indent=4, separators=(',',':')))
 
-rjson = r.json()
+# rjson = r.json()
 
-for key,val in rjson.items():
-	print(str(key) + ':' + str(val))
+total_grand_hours = r.json()['total_grand']/3600000
 
-total_grand_hours = rjson['total_grand']/3600000
-
+print(str(yesterday) + ':' + str(total_grand_hours))
